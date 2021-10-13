5a133762465df34b74b8723c92d1041cecc0293c
Pratiksha96 <jainpratiksha2110@gmail.com>
2021-10-13 03:22:32 -0400

    Basic contact us form plugin

---
+++ contact-us-form.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ *Plugin Name: Contact Form Plugin
+ *Description: This plugin enables user to use this form to contact the IFAS team for any queries
+ **/
+
+register_activation_hook(__FILE__, 'on_activate');
+
+function on_activate()
+{
+    global $wpdb;
+    $create_table_query = "
+            CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}queries` (
+              `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+              `name` varchar(255) NOT NULL,
+              `email` varchar(255) NOT NULL,
+              `phone_number` text NOT NULL,
+              `query` text NOT NULL,
+              `author_IP` text,
+              `time` datetime
+            ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+    ";
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($create_table_query);
+}
+
+function contact_form()
+{
+    /* content variable */
+    $content = '';
+
+    $content .= '<form method="post" action="https://localhost/test-site/?page_id=12">';
+
+    $content .= '<input type="text" required name="full_name" placeholder="Your Full Name" />';
+    $content .= '<br />';
+
+    $content .= '<input type="email" required name="email_address" placeholder="Email Address" />';
+    $content .= '<br />';
+
+    $content .= '<input type="tel" required name="phone_number" placeholder="Phone Number" />';
+    $content .= '<br />';
+
+    $content .= '<textarea name="query" required placeholder="Please enter your query"></textarea>';
+    $content .= '<br />';
+
+    $content .= '<input type="submit" name="submit_form" value="SUBMIT THE INFORMATION" />';
+
+    $content .= '</form>';
+
+    return $content;
+}
+add_shortcode('contact_form', 'contact_form');
+
+
+function set_html_content_type()
+{
+    return 'text/html';
+}
+
+function form_capture()
+{
+    global $wpdb;
+    $tablename = $wpdb->prefix .'queries';
+    $time = current_time('mysql');
+
+    if (array_key_exists('submit_form', $_POST)) {
+        $to = "pr.jain@ufl.edu";
+        $subject = "Testing plugin";
+        $body = '';
+
+        $body .= 'Name: ' . $_POST['full_name'] . ' <br /> ';
+        $body .= 'Email: ' . $_POST['email_address'] . ' <br /> ';
+        $body .= 'Phone: ' . $_POST['phone_number'] . ' <br /> ';
+        $body .= 'Query: ' . $_POST['query'] . ' <br /> ';
+
+
+        add_filter('wp_mail_content_type', 'set_html_content_type');
+
+        wp_mail($to, $subject, $body);
+
+        remove_filter('wp_mail_content_type', 'set_html_content_type');
+    }
+
+    if ( isset( $_POST['submit_form'] ) ){    
+    
+        $data=array(
+            'name' => $_POST['full_name'],
+            'email' => $_POST['email_address'],
+            'phone_number' => $_POST['phone_number'],
+            'query' => $_POST['query'],
+            'author_IP' => $_SERVER['REMOTE_ADDR'],
+            'submission_date' => $time
+        );
+    
+         $wpdb->insert( $tablename, $data);
+    }
+    
+        /* Insert the information into a comment */
+
+    //     $time = current_time('mysql');
+
+    //     $data = array(
+    //         'name' => $_POST['full_name'],
+    //         'email' => $_POST['email_address'],
+    //         'phone_number' => $_POST['phone_number'],
+    //         'query' => $_POST['query'],
+    //         'author_IP' => $_SERVER['REMOTE_ADDR'],
+    //         'submission_date' => $time,
+    //     );
+
+    //     $wpdb->insert($tablename, $data);
+    // }
+}
+add_action('wp_head', 'form_capture');
